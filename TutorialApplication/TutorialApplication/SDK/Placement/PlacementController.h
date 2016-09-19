1266b8d1266f174420d9c74c12d8c741698caa12
MrWooJ <hurricanc@gmail.com>
2016-09-19 20:07:44 +0430

    Enhance Functionality of PlacementController
    * Set Block Actions for Placement
    * Improve Show/Hide Functionality
    * Event Listener

--- TutorialApplication/TutorialApplication/SDK/Placement/PlacementController.h
+++ TutorialApplication/TutorialApplication/SDK/Placement/PlacementController.h
@@ -7,19 +7,50 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PlacementModel.h"
 #import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, Event)
+{
+	PreLoad = 1,
+	DidLoad = 2,
+	FailedLoad = 3,
+	WillAppear = 4,
+	DidAppear = 5,
+	WillDisappear = 6,
+	DidDisappear = 7
+	
+} eventListener;
 
 @interface PlacementController : NSObject
 
-- (nullable id)init;
+- (nullable id)initWithHashID:(nullable NSString *)hashID;
+
+- (void)setDefaultPosition:(CGPoint) point;
 
 - (void)setParentView:(nonnull UIView *)view;
 - (void)setParentViewController:(nonnull UIViewController *)viewController;
 
-- (void)setPosition:(CGPoint) point;
-- (void)setSize:(CGSize) size;
-- (void)setRect:(CGRect) rect;
+- (void)parseBackendResponse:(nonnull NSDictionary *)response;
 
-- (void)setContentFromFileName:(nonnull NSString *)fileName;
+- (void)addOnlineContetWithFileName:(nonnull NSString *)fileName;
+- (void)addOfflineContetWithFileName:(nonnull NSString *)fileName;
+
+- (void)setPreLoadBlock:(nullable void (^)(NSDictionary * _Nonnull details)) preLoadBlock;
+- (void)setDidLoadBlock:(nullable void (^)(NSDictionary * _Nonnull details)) didLoadBlock;
+- (void)setFailedLoadBlock:(nullable void (^)(NSDictionary * _Nonnull details)) failedLoadBlock;
+- (void)setWillAppearBlock:(nullable void (^)(NSDictionary * _Nonnull details)) willAppearBlock;
+- (void)setDidAppearBlock:(nullable void (^)(NSDictionary * _Nonnull details)) didAppearBlock;
+- (void)setWillDisappearBlock:(nullable void (^)(NSDictionary * _Nonnull details)) willDisappearBlock;
+- (void)SetDidDisappearBlock:(nullable void (^)(NSDictionary * _Nonnull details)) didDisappearBlock;
+
+- (void)eventListener:(Event)eventAction Details:(nonnull NSDictionary *) details;
+
+- (void)showOnlineContent;
+- (void)showOfflineContent;
+
+- (void)hideContent;
+
+- (void)removePlacement;
 
 @end
