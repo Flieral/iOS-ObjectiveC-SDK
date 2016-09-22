1a4332d6e03d74e20540a1258edc9980419ac03c
MrWooJ <hurricanc@gmail.com>
2016-09-22 11:57:37 +0330

    Implement Monitoring Strategy inside SDK Engine

--- TutorialApplication/TutorialApplication/SDK/Engine/FlieralSDKEngine.m
+++ TutorialApplication/TutorialApplication/SDK/Engine/FlieralSDKEngine.m
@@ -9,6 +9,7 @@
 #import "FlieralSDKEngine.h"
 #import "FlieralPlacementManager.h"
 #import "APIManager.h"
+#import "MonitorManager.h"
 
 #define PLACEMENTHASHIDKEY		@"FlieralPlacementHashIDKey"
 #define PLACEMENTPRIORTYKEY		@"FlieralPlacementPriorityKey"
@@ -18,16 +19,20 @@
 #define STORAGEAUTHENTICATIONKEY	@"FlieralStoragePublisherHashIDKey"
 #define STORAGEOFFLINEMODEKEY		@"FlieralSTORAGEOfflineModeKey"
 #define STORAGEONLINEMODEKEY		@"FlieralSTORAGEOnlineModeKey"
+#define STORAGEMONITORINGKEY		@"FlieralSTORAGEMonitoringKey"
 
-#define JSONAUTHENTICATIONKEY		@"FlieralJSONAuthenticationKey"
-#define JSONOFFLINEMODEKEY			@"FlieralJSONOfflineModeKey"
-#define JSONONLINEMODEKEY			@"FlieralJSONOnlineModeKey"
+#define JSONAUTHENTICATIONKEY		@"Authentication"
+#define JSONOFFLINEMODEKEY			@"OfflineMode"
+#define JSONONLINEMODEKEY			@"OnlineMode"
+#define JSONMONITORINGKEY			@"Monitoring"
 
-@interface FlieralSDKEngine ()
+@interface FlieralSDKEngine () <MonitorManagerDelegate>
 
 @property (nonatomic, strong) NSMutableArray *placementHashIdArray;
 @property (nonatomic, strong) NSMutableArray *placementManagerArray;
+@property (nonatomic, strong) MonitorManager *monitor;
 
+@property (nonatomic) BOOL readyForStart;
 @property (nonatomic) BOOL readyForUse;
 
 @end
@@ -47,7 +52,10 @@
 		sharedService.placementHashIdArray = [NSMutableArray array];
 		sharedService.placementManagerArray = [NSMutableArray array];
 		sharedService.readyForUse = false;
+		sharedService.readyForStart = false;
 		
+		sharedService.monitor = [[MonitorManager alloc] init];
+		[sharedService.monitor startPinging];
 	});
 	
 	return sharedService;
@@ -83,8 +91,7 @@
 	
 	[_placementHashIdArray addObject:dict];
 	
-	FlieralPlacementManager *placementManager = [[FlieralPlacementManager alloc] init];
-	[placementManager setPlacementHashID:placementHashID];
+	FlieralPlacementManager *placementManager = [[FlieralPlacementManager alloc] initWithHashID:placementHashID];
 	[_placementManagerArray addObject:placementManager];
 }
 
@@ -97,8 +104,7 @@
 	
 	[_placementHashIdArray addObject:dict];
 	
-	FlieralPlacementManager *placementManager = [[FlieralPlacementManager alloc] init];
-	[placementManager setPlacementHashID:placementHashID];
+	FlieralPlacementManager *placementManager = [[FlieralPlacementManager alloc] initWithHashID:placementHashID];
 	[_placementManagerArray addObject:placementManager];
 }
 
@@ -111,8 +117,7 @@
 	
 	[_placementHashIdArray addObject:dict];
 	
-	FlieralPlacementManager *placementManager = [[FlieralPlacementManager alloc] init];
-	[placementManager setPlacementHashID:placementHashID];
+	FlieralPlacementManager *placementManager = [[FlieralPlacementManager alloc] initWithHashID:placementHashID];
 	[_placementManagerArray addObject:placementManager];
 }
 
@@ -120,35 +125,42 @@
 
 - (void)StartEngine
 {
-	NSDictionary *jsonObject = [NSDictionary dictionary];
-	
-	NSDictionary *authRequestContent = [self GetAuthenticationCredential];
-	if (![authRequestContent isEqual:NULL])
-		[jsonObject setValue:authRequestContent forKey:JSONAUTHENTICATIONKEY];
-	
-	NSDictionary *firstRequestContent = [self RetriveOfflineInformation];
-	if (![firstRequestContent isEqual:NULL])
-		[jsonObject setValue:firstRequestContent forKey:JSONOFFLINEMODEKEY];
-	
-	NSDictionary *secondRequestContent = [self RetriveOnlineInformation];
-	if (![secondRequestContent isEqual:NULL])
-		[jsonObject setValue:secondRequestContent forKey:JSONONLINEMODEKEY];
-	
-	[APIManager sendInformationToBackend:jsonObject SuccessBlock:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+	if (_readyForStart)
+	{
+		NSDictionary *jsonObject = [NSDictionary dictionary];
 		
-		NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
-		if (httpResponse.statusCode == 200)
-		{
-			for (FlieralPlacementManager *plManager in _placementManagerArray) {
-				
+		NSDictionary *authRequestContent = [self GetAuthenticationCredential];
+		if (![authRequestContent isEqual:NULL])
+			[jsonObject setValue:authRequestContent forKey:JSONAUTHENTICATIONKEY];
+		
+		NSDictionary *firstRequestContent = [self RetriveOfflineInformation];
+		if (![firstRequestContent isEqual:NULL])
+			[jsonObject setValue:firstRequestContent forKey:JSONOFFLINEMODEKEY];
+		
+		NSDictionary *secondRequestContent = [self RetriveOnlineInformation];
+		if (![secondRequestContent isEqual:NULL])
+			[jsonObject setValue:secondRequestContent forKey:JSONONLINEMODEKEY];
+		
+		NSDictionary *thirdRequestContent = [self RetriveMonitoringInformation];
+		if (![thirdRequestContent isEqual:NULL])
+			[jsonObject setValue:thirdRequestContent forKey:JSONMONITORINGKEY];
+		
+		[APIManager sendInformationToBackend:jsonObject SuccessBlock:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+			
+			NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+			if (httpResponse.statusCode == 200)
+			{
+				for (FlieralPlacementManager *plManager in _placementManagerArray) {
+					
+				}
+				_readyForUse = true;
 			}
-			_readyForUse = true;
-		}
-		
-	} failedBlock:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
-		
-		_readyForUse = false;
-	}];
+			
+		} failedBlock:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
+			
+			_readyForUse = false;
+		}];
+	}
 }
 
 #pragma mark - Placement Manager
@@ -157,11 +169,14 @@
 {
 	if (_readyForUse)
 	{
-		
+		int index = 0;
+		for (NSDictionary *dict in _placementHashIdArray) {
+			if ([[dict valueForKey:PLACEMENTHASHIDKEY] isEqualToString:placementHashID])
+				[_placementManagerArray objectAtIndex:index];
+			index ++;
+		}
 	}
-	if (index < 0 || index >= _placementHashIdArray.count)
-		return NULL;
-	return [_placementHashIdArray objectAtIndex:index];
+	return nil;
 }
 
 #pragma mark - Online Setting
@@ -182,6 +197,24 @@
 	NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
 	dict = [ud objectForKey:STORAGEOFFLINEMODEKEY];
 	return dict;
+}
+
+#pragma mark - Monitoring Setting
+
+- (NSDictionary *)RetriveMonitoringInformation
+{
+	NSDictionary *dict = [NSDictionary dictionary];
+	NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+	dict = [ud objectForKey:STORAGEMONITORINGKEY];
+	return dict;
+}
+
+#pragma mark - Monitoring Delegate
+
+- (void)monitoringCompleteObject:(nonnull NSDictionary *)monitor
+{
+	_readyForStart = true;
+	[self StartEngine];
 }
 
 @end
