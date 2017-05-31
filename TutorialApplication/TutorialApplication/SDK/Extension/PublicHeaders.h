a51b11dce33b22d2430e1e1f856ff407826a9e1b
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-05-31 19:44:36 +0430

    Add PublicHeaders.h for Sharing Pre Define Macros

---
+++ TutorialApplication/TutorialApplication/SDK/Extension/PublicHeaders.h
@@ -0,0 +1,35 @@
+//
+//  PublicHeaders.h
+//  TutorialApplication
+//
+//  Created by Alirëza WJ Arabi on 5/30/17.
+//  Copyright © 2017 Flieral Inc. All rights reserved.
+//
+
+#ifndef PublicHeaders_h
+#define PublicHeaders_h
+
+#define FLPLACEMENTHASHIDKEY            @"Flieral:Placement:HashIDKey"
+#define FLPUBLISHERHASHIDKEY            @"Flieral:Publisher:HashIDKey"
+#define FLAPPLICATIONHASHIDKEY          @"Flieral:Application:HashIDKey"
+
+#define FLSTORAGESDKCACHEKEY            @"Flieral:StorageSDK:CacheKey"
+
+#define FLUSERHASHID                    @"Flieral:User:HashIDKey"
+
+#define FLSTORAGETIMERKEY               @"Flieral:Placement:Timer:"
+#define FLSTORAGEHIDDENKEY              @"Flieral:Placement:IsHidden:"
+
+#define FLDEFAULTTIMER                  60
+
+#define FLPRELOADVIEW                   @"Flieral:View:Preload"
+#define FLDIDLOADVIEW                   @"Flieral:View:Didload"
+#define FLFAILEDLOADVIEW                @"Flieral:View:Failedload"
+#define FLWILLAPPEARVIEW                @"Flieral:View:WillAppear"
+#define FLDIDAPPEARVIEW                 @"Flieral:View:DidAppear"
+#define FLWILLDISAPPEARVIEW             @"Flieral:View:WillDisappear"
+#define FLDIDDISAPPEARVIEW              @"Flieral:View:DidDisappear"
+
+#define FLPLACEMENTMANAGERSTORAGE       @"Flieral:PlacementManager"
+
+#endif
