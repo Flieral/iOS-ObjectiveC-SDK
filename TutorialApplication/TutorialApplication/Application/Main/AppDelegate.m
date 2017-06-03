764af8379002dfb955db7ad6d02218239dc1245b
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-06-03 17:32:37 +0430

    Configure SDK in Beginning of Program and Start Engine

--- TutorialApplication/TutorialApplication/Application/Main/AppDelegate.m
+++ TutorialApplication/TutorialApplication/Application/Main/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FlieralSDKEngine.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,19 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
+	
+    NSDictionary *dict = [NSDictionary dictionary];
+    [dict setValue:@"logEnable" forKey:@"true"];
+    
+    [[FlieralSDKEngine SDKEngine] AddSetting:dict];
+    [[FlieralSDKEngine SDKEngine] Authenticate:@"" ApplicationHashID:@""];
+    
+    [[FlieralSDKEngine SDKEngine] AddPlacement:@""];
+    [[FlieralSDKEngine SDKEngine] AddPlacement:@""];
+    [[FlieralSDKEngine SDKEngine] AddPlacement:@""];
+
+    [[FlieralSDKEngine SDKEngine] StartEngine];
+    
 	return YES;
 }
 
