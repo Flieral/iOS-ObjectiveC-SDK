9739ee2ba7fb8f519053a3f03eb2153505ab6f73
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-06-03 17:34:25 +0430

    Add Placement Management System for Showing on Button Clicks

--- TutorialApplication/TutorialApplication/Application/ViewController/ViewController.m
+++ TutorialApplication/TutorialApplication/Application/ViewController/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "FlieralSDKEngine.h"
 
 @interface ViewController ()
 
@@ -14,21 +15,136 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    [self configuringFirstPlacement];
+    [self configuringSecondPlacement];
+    [self configuringThirdPlacement];
 }
 
-
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
 	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Settings
 
-- (IBAction)firstButtonClicked:(id)sender {
+- (void)configuringFirstPlacement
+{
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetParentViewController:self];
+
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetPreLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: Preload");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: DidLoad");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetFailedLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: FailedLoad");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetWillAppearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: WillAppear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidAppearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: DidAppear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetWillDisappearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: WillDisappear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidDisappearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #1 - Trigger Fired: DidDisappear");
+    }];
 }
 
-- (IBAction)secondButtonClicked:(id)sender {
+- (void)configuringSecondPlacement
+{
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetParentViewController:self];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetPreLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: Preload");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: DidLoad");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetFailedLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: FailedLoad");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetWillAppearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: WillAppear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidAppearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: DidAppear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetWillDisappearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: WillDisappear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidDisappearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #2 - Trigger Fired: DidDisappear");
+    }];
 }
+
+- (void)configuringThirdPlacement
+{
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetParentViewController:self];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetPreLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: Preload");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: DidLoad");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetFailedLoadBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: FailedLoad");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetWillAppearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: WillAppear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidAppearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: DidAppear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetWillDisappearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: WillDisappear");
+    }];
+    
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] SetDidDisappearBlock:^(NSDictionary * _Nonnull details) {
+        NSLog(@"Inside Placement #3 - Trigger Fired: DidDisappear");
+    }];
+}
+
+#pragma mark - Button Handlers
+
+- (IBAction)firstButtonClicked:(id)sender
+{
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] Show];
+}
+
+- (IBAction)secondButtonClicked:(id)sender
+{
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] Show];
+}
+
+- (IBAction)thirdButtonClicked:(id)sender
+{
+    [[[FlieralSDKEngine SDKEngine] GetPlacementWithHashID:@""] Show];
+}
+
 @end
